d258cee613369fd531ce086edef991b1dc1ed724
Yuta Takahashi <takahashi-yuta@kadokawa.jp>
2023-05-03 07:39:21 +0900

    技術スタックの項目追記

--- components/Expertise.vue
+++ components/Expertise.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="mt-8">
+    <h2
+      class="mt-6 text-3xl text-gray-700 font-bold capitalize dark:text-blue-200"
+    >
+      技術スタック
+    </h2>
     <div class="mt-2">
       <h2
         class="mb-2 text-xl text-gray-700 font-bold dark:text-blue-200 capitalize"
@@ -16,19 +21,10 @@
           <Css class="h-10 w-10 rounded-sm drop-shadow-sm"></Css>
         </li> -->
         <li class="flex items-center">
-          <Bootstrap class="h-10 w-10"></Bootstrap>
-        </li>
-        <li class="flex items-center">
-          <TailwindCss class="h-10 w-10"></TailwindCss>
-        </li>
-        <li class="flex items-center">
           <Javascript class="h-10 w-10"></Javascript>
         </li>
         <li class="flex items-center">
-          <Vuejs class="h-10 w-10"></Vuejs>
-        </li>
-        <li class="flex items-center">
-          <Nuxt class="h-10 w-10"></Nuxt>
+          <Bootstrap class="h-10 w-10"></Bootstrap>
         </li>
       </ul>
     </div>
@@ -40,7 +36,19 @@
       </h2>
       <ul class="mb-6 flex flex-wrap gap-4">
         <li class="flex items-center">
-          <Laravel class="h-10 w-10"></Laravel>
+          <Python class="h-10 w-10"></Python>
+        </li>
+        <li class="flex items-center">
+          <Django class="h-10 w-10"></Django>
+        </li>
+        <li class="flex items-center">
+          <Aws class="h-10 w-10"></Aws>
+        </li>
+        <li class="flex items-center">
+          <Oracle class="h-10 w-10"></Oracle>
+        </li>
+        <li class="flex items-center">
+          <Datadog class="h-10 w-10"></Datadog>
         </li>
         <li class="flex items-center">
           <Node class="h-10 w-10"></Node>
@@ -60,6 +68,12 @@ import Nuxt from "../assets/devicon/nuxt.svg?inline";
 import Bootstrap from "../assets/devicon/bootstrap.svg?inline";
 import Laravel from "../assets/devicon/laravel.svg?inline";
 import Node from "../assets/devicon/node.svg?inline";
+import Python from "../assets/devicon/icons8-python.svg?inline";
+import Django from "../assets/devicon/django-logo-negative.svg?inline";
+import Aws from "../assets/devicon/icons8-aws.svg?inline";
+import Oracle from "../assets/devicon/icons8-oracle.svg?inline";
+import Datadog from "../assets/devicon/datadoghq-icon.svg?inline";
+
 export default {
   components: {
     Javascript,
@@ -71,6 +85,11 @@ export default {
     Nuxt,
     Laravel,
     Node,
+    Python,
+    Django,
+    Aws,
+    Oracle,
+    Datadog
   },
 };
 </script>
