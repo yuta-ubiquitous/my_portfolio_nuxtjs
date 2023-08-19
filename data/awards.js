e701433671e5d88fb9d62c51f066290507648bc2
yuta-ubiquitous <yuta-ubiquitous@outlook.jp>
2023-08-20 03:03:15 +0900

    「受賞」の項目追加

---
+++ data/awards.js
@@ -0,0 +1,208 @@
+const awards = [
+  {
+    name: "Best Demonstration Award, IEEE International Conference on Pervasive Computing and Communications (PerCom2019)",
+    date: "2019/03",
+    links: [
+      {
+        title: "公式Facebook",
+        url:"https://www.facebook.com/percom/posts/pfbid0wWBaJQxdvrLn97ziYv7mzL4drd789TwNPpmszAHrximiQ8Zj7qymkoGAaWJmxwPLl"
+      },
+      {
+        title: "NAIST受賞",
+        url:"http://isw3.naist.jp/IS/PubWG/Events-ja/award20190313_ubi.html"
+      },
+    ]
+  },
+  {
+    name: "情報処理学会 第90回 モバイルコンピューティングとパーベイシブシステム研究会 優秀発表賞",
+    date: "2019/03",
+    links: [
+      {
+        title: "学会ページ",
+        url:"https://mbl.ipsj.or.jp/excellent/2018/"
+      },
+    ]
+  },
+  {
+    name: "Best Student Paper Award, The Tenth International Conference on Mobile Computing and Ubiquitous Networking (ICMU 2017)",
+    date: "2017/10",
+    links: [
+      {
+        title: "学会ページ",
+        url:"https://www.icmu.org/icmu2017/?awards"
+      },
+      {
+        title: "NAIST受賞",
+        url:"https://isw3.naist.jp/IS/PubWG/Events-ja/award20171005_takahashi.html"
+      },
+    ]
+  },
+  {
+    name: "情報処理学会 関西支部 2017年度支部大会 支部大会奨励賞",
+    date: "2017/09",
+    links: [
+      {
+        title: "学会ページ",
+        url:"https://kansai.ipsj.or.jp/record/2017.html"
+      },
+      {
+        title: "NAIST受賞",
+        url:"https://isw3.naist.jp/IS/PubWG/Events-ja/award20170925_tokuda.html"
+      },
+    ]
+  },
+  {
+    name: "第82回モバイルコンピューティングとパーペイシブシステム研究会 Work in Progress奨励賞",
+    date: "2016/12",
+    links: [
+      {
+        title: "学会ページ",
+        url:"https://mbl.ipsj.or.jp/excellent/2016"
+      },
+      {
+        title: "NAIST受賞",
+        url:"https://isw3.naist.jp/IS/PubWG/Events-ja/award20161207_takahashi.html"
+      },
+    ]
+  },
+  {
+    name: "e-ZUKAスマートフォンアプリコンテスト2017 Hamee賞",
+    date: "2017/11",
+    links: [
+      {
+        title: "結果ページ",
+        url:"http://e-zuka.info/2017/resurt.html"
+      },
+    ]
+  },
+  {
+    name: "e-ZUKAスマートフォンアプリコンテスト2017 富士通賞",
+    date: "2017/11",
+    links: [
+      {
+        title: "結果ページ",
+        url:"http://e-zuka.info/2017/resurt.html"
+      },
+    ]
+  },
+  {
+    name: "SPAJAM2017 本選 ファイナリスト賞",
+    date: "2017/07",
+    links: [
+      {
+        title: "結果ページ",
+        url:"https://history.spajam.jp/2017/final/result/"
+      },
+      {
+        title: "NAIST受賞",
+        url:"http://isw3.naist.jp/IS/PubWG/Events-ja/award20170710_labol.html"
+      },
+    ]
+  },
+  {
+    name: "SPAJAM2017 大阪予選 優秀賞",
+    date: "2017/07",
+    links: [
+      {
+        title: "結果ページ",
+        url:"https://history.spajam.jp/2017/entry/osaka"
+      },
+      {
+        title: "NAIST受賞",
+        url:"http://isw3.naist.jp/IS/PubWG/Events-ja/award20170423_takahashi.html"
+      },
+    ]
+  },
+  {
+    name: "TechUP Makers グランプリ",
+    date: "2017/02",
+    links: [
+      {
+        title: "イベントページ",
+        url:"https://peatix.com/event/230373"
+      },
+      {
+        title: "NAIST受賞",
+        url:"http://isw3.naist.jp/IS/PubWG/Events-ja/award20170209_takahashi.html"
+      },
+    ]
+  },
+  {
+    name: "TechUP Makers 沖電気工業賞",
+    date: "2017/02",
+    links: [
+      {
+        title: "イベントページ",
+        url:"https://peatix.com/event/230373"
+      },
+      {
+        title: "NAIST受賞",
+        url:"http://isw3.naist.jp/IS/PubWG/Events-ja/award20170209_kido.html"
+      },
+    ]
+  },
+  {
+    name: "第3回ビジネスモデル発見＆発表会 近畿大会 アイ・オー・データ賞",
+    date: "2016/12",
+    links: [
+      {
+        title: "結果ページ",
+        url:"https://ict-business.jp/%ef%bc%92%ef%bc%90%ef%bc%91%ef%bc%96%e3%83%93%e3%82%b8%e3%83%8d%e3%82%b9%e3%83%a2%e3%83%87%e3%83%ab%e7%99%ba%e8%a6%8b%ef%bc%86%e7%99%ba%e8%a1%a8%e4%bc%9a-%e8%bf%91%e7%95%bf%e5%a4%a7%e4%bc%9a%e3%80%90/"
+      },
+      {
+        title: "NAIST受賞",
+        url:"http://isw3.naist.jp/IS/PubWG/Events-ja/award20161209_kido.html"
+      },
+    ]
+  },
+  {
+    name: "e-ZUKAスマートフォンアプリコンテスト2016 優秀技術賞(ハウインターナショナル賞)",
+    date: "2016/11",
+    links: [
+      {
+        title: "結果ページ",
+        url:"http://e-zuka.info/2016/resurt.html"
+      },
+      {
+        title: "NAIST受賞",
+        url:"http://isw3.naist.jp/IS/PubWG/Events-ja/award20161119_hersch.html"
+      },
+    ]
+  },
+  {
+    name: "e-ZUKAスマートフォンアプリコンテスト2016 IDCフロンティア賞",
+    date: "2016/11",
+    links: [
+      {
+        title: "結果ページ",
+        url:"http://e-zuka.info/2016/resurt.html"
+      },
+      {
+        title: "NAIST受賞",
+        url:"http://isw3.naist.jp/IS/PubWG/Events-ja/award20161119_hersch.html"
+      },
+    ]
+  },
+  {
+    name: "全国高等専門学校プログラミングコンテスト 第24回旭川大会 課題部門 敢闘賞",
+    date: "2013/10",
+    links: [
+      {
+        title: "結果ページ",
+        url:"https://www.procon.gr.jp/wp-content/uploads/2016/10/24_honsenkekka.pdf"
+      },
+    ]
+  },
+  {
+    name: "平成30年度奈良先端科学技術大学院大学優秀学生",
+    date: "2018/06",
+    links: [
+      {
+        title: "結果ページ",
+        url:"https://www.naist.jp/news/2018/07/005151.html"
+      },
+    ]
+  },
+];
+
+export default awards;
